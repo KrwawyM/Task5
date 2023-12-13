88b69f3b4dc78b5a59ab870eff5f44da31fc2924
wikto <wikto@KOMPWIKI>
2023-12-13 13:59:33 +0100

    Add project files.

---
+++ Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoje imię:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje nazwisko:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoj nr telefonu:");
+            int phone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj swoj adres email:");
+            string eamil = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoj wzrost w cm:");
+            int high = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj swoją wagę:");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ile metrów kw ma twoje mieszkanie?");
+            double area = double.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
